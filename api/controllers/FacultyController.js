1580bef54a04d6b25d18a952ff3017b42e15198e
Karmasakshi Goyal <karmasakshi121@gmail.com>
2016-08-04 22:33:59 +0530

    Add APIs.

---
+++ api/controllers/FacultyController.js
@@ -0,0 +1,11 @@
+/**
+ * FacultyController
+ *
+ * @description :: Server-side logic for managing faculties
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/LectureController.js
@@ -0,0 +1,11 @@
+/**
+ * LectureController
+ *
+ * @description :: Server-side logic for managing lectures
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
